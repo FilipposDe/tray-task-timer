f4ca1660668cbed8b2415dbeb7f0cbcc3d0f4e9f
1awe23 <legolas19902001@yahoo.gr>
2020-12-12 03:04:21 +0200

    initial

--- main.js
+++ main.js
@@ -1,43 +1,216 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Tray, Menu, ipcMain: ipc } = require( 'electron' )
+const fs = require( 'fs' )
+
+let mainWindow = null
+let tray = null
+
+let currentTimerIndex = -1
+let interval
+let start
+
+const timers = []
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+	mainWindow = new BrowserWindow( {
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		}
+	} )
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+	mainWindow.loadFile( 'index.html' )
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	mainWindow.on( 'minimize', e => {
+		e.preventDefault();
+		mainWindow.hide();
+	} )
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+app.whenReady().then( () => {
+	createWindow()
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+	const loadedTimers = loadJSON()
+	if ( loadedTimers ) {
+		timers.splice( 0, timers.length )
+		loadedTimers.forEach( timer => {
+			timers.push( {
+				title: timer.title,
+				time: timer.time,
+				isPaused: true,
+			} )
+		} );
+	}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+	mainWindow.webContents.on( 'did-finish-load', () => {
+		mainWindow.webContents.send( 'loadTimers', timers )
+	} )
+
+	// mainWindow.webContents.openDevTools()
+
+
+	tray = new Tray( 'icon.jpg' )
+
+	tray.setToolTip( 'Timer' )
+	updateTrayMenu()
+
+
+	ipc.handle( 'delete', ( event, index ) => {
+		if ( currentTimerIndex === index ) return false
+		timers.splice( index, 1 )
+		return true
+	} )
+
+	ipc.handle( 'rename', ( event, index, name ) => {
+		timers[ index ].title = name
+		updateTrayMenu()
+		return true
+	} )
+
+	ipc.on( 'create', ( event, newName ) => {
+		timers.push( {
+			title: newName,
+			time: 0,
+			isPaused: false,
+		} )
+		mainWindow.webContents.send( 'loadTimers', timers )
+	} )
+
+
+	mainWindow.on( 'close', () => {
+		clearInterval( interval )
+		saveJSON()
+	} )
+
+
+
+	app.on( 'activate', function () {
+		if ( BrowserWindow.getAllWindows().length === 0 ) createWindow()
+	} )
+
+} )
+
+
+app.on( 'window-all-closed', function () {
+	if ( process.platform !== 'darwin' ) app.quit()
+} )
+
+
+function startTimer ( timerIndex ) {
+	if ( timers[ timerIndex ].isPaused ) {
+		start = Date.now() - timers[ timerIndex ].time
+	} else {
+		start = Date.now()
+	}
+	currentTimerIndex = timerIndex
+	interval = setInterval( () => {
+		timers[ timerIndex ].time = Date.now() - start
+		mainWindow.webContents.send( 'timerUpdate' + timerIndex, timers[ timerIndex ].time )
+		updateTrayMenu()
+	}, 1000 )
+}
+
+function pauseTimer ( timerIndex ) {
+	clearInterval( interval )
+	timers[ timerIndex ].isPaused = true
+}
+
+function stopTimer ( timerIndex ) {
+	clearInterval( interval )
+	timers[ timerIndex ].time = 0
+	start = null
+	timers[ timerIndex ].isPaused = false
+	mainWindow.webContents.send( 'timerUpdate' + timerIndex, timers[ timerIndex ].time )
+	currentTimerIndex = -1
+	updateTrayMenu()
+}
+
+
+
+function updateTrayMenu () {
+
+	const menuItems = []
+
+	menuItems.push( {
+		label: currentTimerIndex === -1 || timers[ currentTimerIndex ].isPaused ? "No timer running" : timers[ currentTimerIndex ].title,
+		enabled: false
+	} )
+
+
+	menuItems.push( {
+		type: 'separator',
+	} )
+
+
+
+	timers.forEach( ( timer, index ) => {
+		menuItems.push( {
+			label: 'Start ' + timer.title,
+			click: () => {
+				timers.forEach( ( otherTimer, otherIndex ) => {
+					if ( otherTimer !== timer ) {
+						pauseTimer( otherIndex )
+					}
+				} )
+				startTimer( index )
+			}
+		} )
+	} )
+
+	menuItems.push( {
+		label: 'Clear All', click: () => {
+			if ( currentTimerIndex !== -1 ) {
+				timers.forEach( ( timer, index ) => {
+					stopTimer( index )
+				} )
+				currentTimerIndex = -1
+				updateTrayMenu()
+				saveJSON()
+			}
+		}
+	} )
+
+
+	menuItems.push( {
+		label: 'Pause', click: () => {
+			if ( currentTimerIndex !== -1 ) {
+				pauseTimer( currentTimerIndex )
+				saveJSON()
+			}
+		}
+	} )
+
+
+	menuItems.push( {
+		label: 'Open App', click: () => mainWindow.show()
+	} )
+
+
+	tray.setContextMenu( Menu.buildFromTemplate( menuItems ) )
+
+}
+
+function saveJSON () {
+
+	const data = JSON.stringify( timers.map( timer => ( {
+		title: timer.title, time: timer.time
+	} ) ) )
+
+	fs.writeFile( 'storage.json', data, err => { } )
+
+}
+
+
+function loadJSON () {
+
+	try {
+		const json = fs.readFileSync( 'storage.json', { encoding: 'utf8' } )
+		return JSON.parse( json )
+	} catch ( error ) {
+		console.error( error )
+		return null
+	}
+
+}
+
